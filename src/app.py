96000b40d62ea3fbb605e8b289a53e959b64d9b8
eperezvazquez <105649443+eperezvazquez@users.noreply.github.com>
2022-08-08 23:15:01 -0300

    Lo realice local pero subo los arhivos generados
    Realize todo el proyecto local subo los archivos generados.

--- src/app.py
+++ src/app.py
@@ -1,4 +1,126 @@
-from utils import db_connect
-engine = db_connect()
+''' importamos las librerias''' 
 
-# your code here
+import numpy as np
+import sys
+import tensorflow as tf
+import os
+import sys
+import pandas as pd
+import keras,os
+import visualkeras
+import itertools
+import keras
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from sklearn.metrics import confusion_matrix
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPool2D , Flatten
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.preprocessing import image
+
+
+#Use the ImageDataGenerator to load the data
+trdata = ImageDataGenerator()
+traindata = trdata.flow_from_directory(directory="../src/",target_size=(224,224))
+tsdata = ImageDataGenerator()
+testdata = tsdata.flow_from_directory(directory="../src/", target_size=(224,224))
+#Pass the data to the neural network
+model = Sequential()
+model.add(Conv2D(input_shape=(224,224,3),filters=64,kernel_size=(3,3),padding="same", activation="relu"))
+model.add(Conv2D(filters=64,kernel_size=(3,3),padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+#Pass the data to the dense layer
+model.add(Flatten())
+model.add(Dense(units=4096,activation="relu"))
+model.add(Dense(units=4096,activation="relu"))
+model.add(Dense(units=2, activation="sigmoid"))
+#Compile the model
+
+opt = Adam(lr=0.001)
+model.compile(optimizer=opt, loss=keras.losses.binary_crossentropy, metrics=['accuracy'])
+
+visualkeras.layered_view(model, legend=True)
+#Create objects for ModelCheckpoint and EarlyStopping, and pass them as callback functions to fit_generator
+
+checkpoint = ModelCheckpoint("vgg16_1.h5", monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', save_freq=1)
+early = EarlyStopping(monitor='val_acc', min_delta=0, patience=20, verbose=1, mode='auto')
+hist=model.fit_generator(steps_per_epoch=5,
+                         generator=traindata, 
+                         validation_data= testdata, 
+                         validation_steps=1,
+                         epochs=5,
+                         callbacks=[checkpoint,early])
+# Evaluacion del Modelo
+
+test_loss, test_acc = model.evaluate(testdata, steps=len(testdata), verbose=1)
+print('Loss: %.3f' % (test_loss * 100.0))
+print('Accuracy: %.3f' % (test_acc * 100.0)) 
+
+predictions = model.predict(x=testdata, steps= len(testdata), verbose=0)
+testdata.classes
+cm = confusion_matrix(y_true=testdata.classes, y_pred=np.argmax(predictions, axis=-1))
+def plot_confusion_matrix(cm, classes,normalize=False,title='Confusion matrix',cmap=plt.cm.Blues):
+    
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+        print(cm)
+    
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+        horizontalalignment="center",
+        color="white" if cm[i, j] > thresh else "black")
+        plt.tight_layout()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+cm_plot_labels = ['0_cat', '1_dog']
+
+plot_confusion_matrix(cm=cm, classes=cm_plot_labels, title='Confusion Matrix')
+#Make predictions
+
+
+#imgage = mpimg.imread('C:/Users/perez/OneDrive/Desktop/ProjectoML/src/test1/1839.jpg')
+image = tf.keras.preprocessing.image.load_img('C:/Users/perez/OneDrive/Desktop/ProjectoML/src/test1/1839.jpg',target_size=(224,224))
+input_arr = tf.keras.preprocessing.image.img_to_array(image)
+input_arr = np.array([input_arr])  # Convert single image to a batch.
+predictions = model.predict(input_arr)
+img = np.asarray(img)
+plt.imshow(img)
+img = np.expand_dims(img, axis=0)
+from keras.models import load_model
+saved_model = load_model("vgg16_1.h5")
+output = saved_model.predict(img)
+if output[0][0] > output[0][1]:
+    print("cat")
+else:
+    print('dog')
+
+
